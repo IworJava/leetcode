4d35e051420abc0da6a1f926ffc6af5da915d286
Iw <iwor.java@gmail.com>
2023-09-08 15:44:04 +0400

    HashMap

--- src/main/java/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,8 @@
 package medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringWithoutRepeatingCharacters {
 
     /*
@@ -17,6 +20,27 @@ public class LongestSubstringWithoutRepeatingCharacters {
                     break;
                 }
             }
+        }
+        return result;
+    }
+
+    /*
+        5 ms, 43.6 MB
+     */
+    public int lengthOfLongestSubstring1(String s) {
+        if (s == null) {
+            return 0;
+        }
+        int len = s.length();
+        int result = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0, j = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c) && map.get(c) >= j) {
+                j = map.get(c) + 1;
+            }
+            result = Math.max(result, i - j + 1);
+            map.put(s.charAt(i), i);
         }
         return result;
     }
--- src/test/java/medium/LongestSubstringWithoutRepeatingCharactersTest.java
+++ src/test/java/medium/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -3,37 +3,62 @@ package medium;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class LongestSubstringWithoutRepeatingCharactersTest {
 
-    private final String str = "abcabcbb";
+    private final LongestSubstringWithoutRepeatingCharacters obj =
+            new LongestSubstringWithoutRepeatingCharacters();
 
     @Test
     void shouldReturnLengthOfLongestSubstring() {
-        int expected = 3;
+        Map<String, Integer> map = new HashMap<>();
+        map.put("abcabcbb", 3);
+        map.put("aab", 2);
+        map.put("dvdf", 3);
+        map.put("tmmzuxt", 5);
+        var expected = map.values().toArray();
 
-        int actual = new LongestSubstringWithoutRepeatingCharacters()
-                .lengthOfLongestSubstring(str);
+        List<Integer> actual = new ArrayList<>();
+        List<Integer> actual1 = new ArrayList<>();
+        map.forEach((k, v) -> actual.add(obj.lengthOfLongestSubstring(k)));
+        map.forEach((k, v) -> actual1.add(obj.lengthOfLongestSubstring1(k)));
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertAll(
+                () -> assertArrayEquals(expected, actual.toArray()),
+                () -> assertArrayEquals(expected, actual1.toArray())
+        );
     }
 
     @Test
     void shouldReturnZeroOnEmptyString() {
         int expected = 0;
 
-        int actual = new LongestSubstringWithoutRepeatingCharacters()
-                .lengthOfLongestSubstring("");
+        int actual = obj.lengthOfLongestSubstring("");
+        int actual1 = obj.lengthOfLongestSubstring1("");
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertAll(
+                () -> assertEquals(expected, actual),
+                () -> assertEquals(expected, actual1)
+        );
     }
 
     @Test
     void shouldReturnZeroOnNull() {
         int expected = 0;
 
-        int actual = new LongestSubstringWithoutRepeatingCharacters()
-                .lengthOfLongestSubstring(null);
+        int actual = obj.lengthOfLongestSubstring(null);
+        int actual1 = obj.lengthOfLongestSubstring1(null);
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertAll(
+                () -> assertEquals(expected, actual),
+                () -> assertEquals(expected, actual1)
+        );
     }
 }
