0f7cb8ff90282e8a243e57c879db7921d997704e
Iw <iwor.java@gmail.com>
2023-09-09 10:40:15 +0400

    String

--- src/main/java/easy/PalindromeNumber.java
+++ src/main/java/easy/PalindromeNumber.java
@@ -9,4 +9,17 @@ public class PalindromeNumber {
         StringBuilder sb = new StringBuilder(String.valueOf(x));
         return sb.reverse().toString().contentEquals(new StringBuilder(String.valueOf(x)));
     }
+
+    /*
+        10 ms, 42.1 MB
+     */
+    public boolean isPalindrome1(int x) {
+        String s = Integer.toString(x);
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/easy/PalindromeNumberTest.java
+++ src/test/java/easy/PalindromeNumberTest.java
@@ -15,27 +15,33 @@ class PalindromeNumberTest {
     @Test
     void shouldReturnTrue() {
         boolean actual = obj.isPalindrome(palindrome);
+        boolean actual1 = obj.isPalindrome1(palindrome);
 
         assertAll(
-                () -> assertTrue(actual)
+                () -> assertTrue(actual),
+                () -> assertTrue(actual1)
         );
     }
 
     @Test
     void shouldReturnFalse() {
         boolean actual = obj.isPalindrome(notPalindrome);
+        boolean actual1 = obj.isPalindrome1(notPalindrome);
 
         assertAll(
-                () -> assertFalse(actual)
+                () -> assertFalse(actual),
+                () -> assertFalse(actual1)
         );
     }
 
     @Test
     void shouldReturnTrueOnZero() {
         boolean actual = obj.isPalindrome(0);
+        boolean actual1 = obj.isPalindrome1(0);
 
         assertAll(
-                () -> assertTrue(actual)
+                () -> assertTrue(actual),
+                () -> assertTrue(actual1)
         );
     }
 }
