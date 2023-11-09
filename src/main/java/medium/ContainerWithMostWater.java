1f69b6d2b8d2cc7b15eb6247807f912c767a1ab7
Iw <iwor.java@gmail.com>
2023-11-09 13:12:45 +0300

    two pointers optimized

--- src/main/java/medium/ContainerWithMostWater.java
+++ src/main/java/medium/ContainerWithMostWater.java
@@ -21,6 +21,34 @@ public class ContainerWithMostWater {
         return max;
     }
 
+    /**
+     * 2 ms, 55.46 MB
+     */
+    public int maxArea1(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int lv = height[l];
+        int rv = height[r];
+        int max = area(height, l, r);
+
+        while (l < r) {
+            if (lv < rv) {
+                l++;
+                if (lv < height[l]) {
+                    lv = height[l];
+                    max = Math.max(max, area(height, l, r));
+                }
+            } else {
+                r--;
+                if (rv < height[r]) {
+                    rv = height[r];
+                    max = Math.max(max, area(height, l, r));
+                }
+            }
+        }
+        return max;
+    }
+
     private int area(int[] height, int left, int right) {
         return (right - left) * Math.min(height[left], height[right]);
     }
--- src/test/java/medium/ContainerWithMostWaterTest.java
+++ src/test/java/medium/ContainerWithMostWaterTest.java
@@ -10,17 +10,21 @@ class ContainerWithMostWaterTest {
 
     @Test
     void shouldReturnCorrectResult() {
-        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-        int[] height1 = {1, 5};
-        int expected = 49;
-        int expected1 = 1;
+        int[] height1 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int[] height2 = {1, 5};
+        int expected1 = 49;
+        int expected2 = 1;
 
-        int actual = obj.maxArea(height);
-        int actual1 = obj.maxArea(height1);
+        int actual11 = obj.maxArea(height1);
+        int actual12 = obj.maxArea(height2);
+        int actual21 = obj.maxArea1(height1);
+        int actual22 = obj.maxArea1(height2);
 
         assertAll(
-                () -> assertEquals(expected, actual),
-                () -> assertEquals(expected1, actual1)
+                () -> assertEquals(expected1, actual11),
+                () -> assertEquals(expected2, actual12),
+                () -> assertEquals(expected1, actual21),
+                () -> assertEquals(expected2, actual22)
         );
     }
 }
